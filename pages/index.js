c009fa1ebf18a9351697ffe4f59202e37455db45
manas <bagrimanasbir@gmail.com>
2022-03-06 23:46:04 -0800

    yeah yeah'

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,10 @@ export default function Home() {
       </Head>
       <h1>manas</h1>
       <p>15</p>
-      <p>interested in crypto, web3, decentralization, meritocracy, among many other things</p>
+      <p>interested in crypto, web3, decentralization, meritocracy, writing, among many other things</p>
       <p>familiar with solidity, nextjs, design programs</p>
       <p>looking for work</p>
+      <p>building and learning</p>
       <p>manas#9437</p>
     </div>
   )
