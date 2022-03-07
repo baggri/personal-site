8aebb2fab60e30afd7feb8b335e5c4cf100080cb
manas <bagrimanasbir@gmail.com>
2022-03-06 23:44:39 -0800

    yeah again

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>manas</title>
+      </Head>
       <h1>manas</h1>
       <p>15</p>
       <p>interested in crypto, web3, decentralization, meritocracy, among many other things</p>
